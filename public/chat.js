198adc435f3ac229fc303105f8a61ea491e6215c
Orshimon810 <127754114+Orshimon810@users.noreply.github.com>
2023-08-23 13:45:23 +0300

    chat html page and js

--- public/chat.js
+++ public/chat.js
@@ -1,20 +1,45 @@
-const socket = io('http://localhost:3000');
+document.addEventListener('DOMContentLoaded', () => {
+    const socket = io('http://localhost:3000');
 
-const messageInput = document.querySelector('input[name="message"]');
-const sendMessageButton = document.querySelector('button');
-const messagesContainer = document.querySelector('.body');
+    const nameInput = document.getElementById('name');
+    const messageInput = document.getElementById('message');
+    const form = document.getElementById('chat-form');
+    const messagesContainer = document.querySelector('.body');
 
-sendMessageButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    const message = messageInput.value;
-    if (message.trim() !== '') {
-        socket.emit('chat message', message);
-        messageInput.value = '';
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const name = nameInput.value.trim();
+        const message = messageInput.value.trim();
+
+        if (name !== '' && message !== '') {
+            // Emit the message along with the sender's name to the server
+            socket.emit('chat message', { name, message });
+
+            // Clear the message input
+            messageInput.value = '';
+        }
+    });
+
+    socket.on('chat message', (data) => {
+        displayMessage(data.name, data.message);
+    });
+
+    function displayMessage(sender, message) {
+        const messageElement = document.createElement('p');
+        
+        const senderElement = document.createElement('span');
+        senderElement.textContent = sender + ': ';
+        senderElement.classList.add('sender-name'); // Apply CSS class to the sender's name
+        
+        const messageTextElement = document.createElement('span');
+        messageTextElement.textContent = message;
+
+        
+        
+        messageElement.appendChild(senderElement);
+        messageElement.appendChild(messageTextElement);
+        
+        messagesContainer.appendChild(messageElement);
     }
-});
-
-socket.on('chat message', (message) => {
-    const messageElement = document.createElement('p');
-    messageElement.textContent = message;
-    messagesContainer.appendChild(messageElement);
 });
