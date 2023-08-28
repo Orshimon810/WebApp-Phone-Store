e12a74b21137eebed93bc948a64d07d29bec2600
Orshimon810 <127754114+Orshimon810@users.noreply.github.com>
2023-08-28 13:19:03 +0300

    Revert "Merge branch 'frontend-v3' into frontend-v3-or"
    This reverts commit 94af8fb4977334b999a7b64bb5be9efe8b17d420, reversing
    changes made to 44e3d5b9f5fc968c1f56a919b15850b3f5c478c4.

--- public/chat.js
+++ public/chat.js
@@ -1,20 +1,60 @@
-const socket = io('http://localhost:3000');
+document.addEventListener('DOMContentLoaded', () => {
+    const socket = io('http://localhost:3000');
 
-const messageInput = document.querySelector('input[name="message"]');
-const sendMessageButton = document.querySelector('button');
-const messagesContainer = document.querySelector('.body');
+    const nameInput = document.getElementById('name');
+    const messageInput = document.getElementById('message');
+    const form = document.getElementById('chat-form');
+    const messagesContainer = document.querySelector('.body');
 
-sendMessageButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    const message = messageInput.value;
-    if (message.trim() !== '') {
-        socket.emit('chat message', message);
-        messageInput.value = '';
+    const nameColors = {}; // Store name-color pairs
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const name = nameInput.value.trim();
+        const message = messageInput.value.trim();
+
+        if (name !== '' && message !== '') {
+            // Emit the message along with the sender's name to the server
+            socket.emit('chat message', { name, message });
+
+            // Clear the message input
+            messageInput.value = '';
+        }
+    });
+
+    socket.on('chat message', (data) => {
+        displayMessage(data.name, data.message);
+    });
+
+    function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
     }
-});
 
-socket.on('chat message', (message) => {
-    const messageElement = document.createElement('p');
-    messageElement.textContent = message;
-    messagesContainer.appendChild(messageElement);
+    function displayMessage(sender, message) {
+        const messageElement = document.createElement('p');
+        
+        const senderElement = document.createElement('span');
+        senderElement.textContent = sender + ': ';
+        
+        // Get or generate a random color for the sender's name
+        if (!nameColors[sender]) {
+            nameColors[sender] = getRandomColor();
+        }
+        senderElement.style.color = nameColors[sender];
+        senderElement.style.fontSize = '22px';
+        
+        const messageTextElement = document.createElement('span');
+        messageTextElement.textContent = message;
+        
+        messageElement.appendChild(senderElement);
+        messageElement.appendChild(messageTextElement);
+        
+        messagesContainer.appendChild(messageElement);
+    }
 });
