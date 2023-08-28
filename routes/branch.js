e4c7da521e99cf79c93fb647719740cf78326fa5
Tal <zigelnik@cs.colman.ac.il>
2023-08-28 13:15:28 +0300

    t

--- routes/branch.js
+++ routes/branch.js
@@ -1,23 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const branchRouter = require('../controllers/branch');
+const branchController = require('../controllers/branch');
 
-//get the all branches
-router.get(`/`, branchRouter.getAllBranches);
-
-//getBranch by ID
-router.get('/:id',branchRouter.getBranch);
-
-//add branch
-router.post(`/`,branchRouter.addBranch);
-
-//api/v1/64d76d49986cc123b199321f
-//delete branch
-router.delete('/:id',branchRouter.deleteBranch);
-
-//update branch
-router.put('/:id',branchRouter.updateBranch)
-
-// get branch's coordnates?
+// Define routes
+router.route('/').get(branchController.getAllBranches).post(branchController.addBranch);
+router.route('/:id').get(branchController.getBranch).delete(branchController.deleteBranch).put(branchController.updateBranch);
 
 module.exports = router;
